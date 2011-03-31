3dfe9c2431493ac918215f8af2d4633b36a76659
psholtz <paul.sholtz@gmail.com>
2011-03-30 22:52:27 -0700

    adding categories

---
+++ Categories/NSString+Reverse.m
@@ -0,0 +1,34 @@
+//
+//  NSString+Reverse.m
+//
+//  Example of how to use Categories in Objective-C.
+//
+//  Example syntax might be something like:
+//
+//   NSString *one = "test string";
+//   NSString *two = [one reverse];
+//   NSLog(@"++ first: %@",one);
+//   NSLog(@"++ second: %@",two);
+//
+
+#import "NSString+Reverse.h"
+
+
+@implementation NSString (reverse)
+
+- (NSString*) reverseString
+{
+	NSMutableString *reversedStr;
+	int len = [self length];
+	
+	// auto released string
+	reversedStr = [NSMutableString stringWithCapacity:len];
+	
+	// quick-and-dirty implementation
+	while ( len > 0 )
+		[reversedStr appendString:[NSString stringWithFormat:@"%C",[self characterAtIndex:--len]]];
+	
+	return reversedStr;
+}
+
+@end
